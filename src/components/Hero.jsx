fb9acddacb853230dff9619542e4cafce1538574
paradoxxxinvader <120823089+paradoxxxinvader@users.noreply.github.com>
2025-09-16 01:30:04 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,11 +18,11 @@ const Hero = () => {
 
         <div className="mt-28 md:mt-5 lg:5">
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Huzaif</span>
+            Hi, I'm <span className='text-[#915EFF]'>Ayush</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop modern, user <br/>
-            interfaces and web applications
+            Software Development Engineer <br/>
+            with expertise in Python, C++, and JavaScript.
           </p>
         </div>
       </div>
