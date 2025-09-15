4754366312bce22b1c1ef369acbcd5b14ad4933c
paradoxxxinvader <120823089+paradoxxxinvader@users.noreply.github.com>
2025-09-16 01:28:53 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -72,9 +72,7 @@ const About = () => {
       </div>
 
       <p ref={paragraphRef} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled software developer with experience in  JavaScript, and expertise in frameworks 
-        like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, 
-        scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
+        Software Development Engineer (SDE) with expertise in Python, C++, JavaScript, and full stack web development. Skilled in building scalable applications, REST APIs, and distributed systems, with experience in debugging, performance optimization, and secure systems design. Strong foundation in data structures, algorithms, and problem-solving, demonstrated through 200+ solved problems on competitive platforms. Passionate about creating impactful, high-performance software solutions.
       </p>
 
       <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-10">
